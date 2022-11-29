bb5c7f8f23b9d2115a842c04a32a60da7d233de2
Pilusimida <84691256+Pilusimida@users.noreply.github.com>
2022-11-29 14:29:57 +0800

    这个解子仅用了随机生成

--- CARP_solver.py
+++ CARP_solver.py
@@ -1,22 +1,20 @@
 # data = np.fromfile("CARP/CARP_samples/egl-e1-A.dat")
 # print(data.shape)
 # print(data)
-import math
 import sys
-
 from util import *
 
 # random.seed(1)
 '''
-这个版本的解子是用的姚新老师的论文：Memetic Algorithm with Extended Neighborhood Search for Capacitated Arc Routing Problems
+这个版本的解子是用的在pathscanning process randomly choose strategy
 '''
 
 if __name__ == "__main__":
     '''
-    从命令昂获取运行参数
+    从命令行获取运行参数
     '''
     address = sys.argv[1]
-    # address = 'CARP_samples/val1A.dat'
+    # address = 'CARP_samples/egl-e1-A.dat'
     t = sys.argv[3]
     # t = 3
     random.seed(sys.argv[5])
@@ -33,7 +31,7 @@ if __name__ == "__main__":
     paths = []
     vertices = information['VERTICES']
     depot = int(information['DEPOT'])
-    required_edges = information['REQUIRED EDGES']
+    required_edges = int(information['REQUIRED EDGES'])
     non_required_edges = information['NON-REQUIRED EDGES']
     vehicles = int(information['VEHICLES'])
     capacity = int(information['CAPACITY'])
@@ -44,49 +42,29 @@ if __name__ == "__main__":
     '''
     f = floyd(graph[1:], int(vertices))
 
-    routes1, chromosome1 = path_scanning(vehicles, depot, capacity, demand, demand_edge, cost, f, 1)
-    routes2, chromosome2 = path_scanning(vehicles, depot, capacity, demand, demand_edge, cost, f, 2)
-    routes3, chromosome3 = path_scanning(vehicles, depot, capacity, demand, demand_edge, cost, f, 3)
-    routes4, chromosome4 = path_scanning(vehicles, depot, capacity, demand, demand_edge, cost, f, 4)
-    routes5, chromosome5 = path_scanning(vehicles, depot, capacity, demand, demand_edge, cost, f, 5)
-    routes6, chromosome6 = path_scanning(vehicles, depot, capacity, demand, demand_edge, cost, f, 6)
 
-    # print_route(chromosome1, edge=demand_edge)
-    c1 = print_calculate_cost(chromosome1, f, depot, cost, demand_edge)
-    # print_cost(c1)
+    chromosomes = []
+    sequences = []
+    for i in range(10000):
+        sequence = []
+        for j in range(vehicles):
+            sequence.append(random.randint(1, 6))
+        while sequence in sequence:
+            sequence = []
+            for j in range(vehicles):
+                sequence.append(random.randint(1, 6))
+        sequences.append(sequence)
+        route_random, chromosome_random = random_path_scanning(vehicles, depot, capacity, demand, demand_edge, cost, f,
+                                                               sequence)
+        c_random = calculate_cost(chromosome_random, f, depot, cost, demand_edge)
+        chromosomes.append((chromosome_random, c_random))
+        if i % 100:
+            chromosomes.sort(key=lambda x: x[1])
+            chromosomes = chromosomes[0:100]
 
-    # print_route(chromosome2, edge=demand_edge)
-    c2 = print_calculate_cost(chromosome2, f, depot, cost, demand_edge)
-    # print_cost(c2)
+    chromosomes.sort(key=lambda x: x[1])
 
-    # print_route(chromosome3, edge=demand_edge)
-    c3 = print_calculate_cost(chromosome3, f, depot, cost, demand_edge)
-    # print_cost(c3)
-
-    # print_route(chromosome4, edge=demand_edge)
-    c4 = print_calculate_cost(chromosome4, f, depot, cost, demand_edge)
-    # print_cost(c3)
-
-    # print_route(chromosome5, edge=demand_edge)
-    c5 = print_calculate_cost(chromosome5, f, depot, cost, demand_edge)
-    # print_cost(c5)
-
-    # print_route(chromosome6, edge=demand_edge)
-    c6 = print_calculate_cost(chromosome6, f, depot, cost, demand_edge)
-    # print_cost(c6)
-
-    # print(chromosome1)
-
-    rank = [(chromosome1, c1), (chromosome2, c2), (chromosome3, c3), (chromosome4, c4), (chromosome5, c5),
-            (chromosome6, c6)]
-    chro = []
-    co = math.inf
-    for r in rank:
-        if r[1] < co:
-            chro = r[0]
-            co = r[1]
-    print_route(chro, demand_edge)
-    print_cost(co)
-
-
-
+    c = chromosomes[0]
+    # print(c)
+    print_route(c[0], demand_edge)
+    print_cost(c[1])
